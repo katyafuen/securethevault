2edf432073b57a0217453b1c3f87f02f902dc995
Katya <katerynarzh@gmail.com>
2025-03-03 21:06:20 -0500

    java script vault combo as an alert

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1. Create a string (const)
+// 2. Assign variable to 3 combo codes using ariphmetic calculations
+// 3. Create an alert showing the string with the vault combo
+
+const message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
+
+const num1 = 5 + 5;
+
+const num2 = 80 / 2;
+
+const num3 = 40 - 1;
+
+// use all 4 variables above to concatenate a string
+
+alert(`${message}: ${num1} - ${num2} - ${num3}`);
